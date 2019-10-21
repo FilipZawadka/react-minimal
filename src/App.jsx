01d7a1738c34e9c3d0537629660cb7cef594dcfb
FilipZawadka <38583399+FilipZawadka@users.noreply.github.com>
2019-10-21 09:49:32 +0200

    Lab2Ex4
    Lab2Ex4

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,40 @@ import AppTitle from './AppTitle'
 import { ENETUNREACH } from 'constants';
 
 function generateArray(a,b) {return Array.from({length:b-a+1} ,(val,index)=>index+a);}
+function generateSquare(arr){return arr.map(function(x){return Math.pow(x,2);})};
+var arr=[];
 let rendnum=0;
+class Toggle extends Component
+{
+  state={
+    on:false,
+  }
+  toggle= ()=>{
+    this.setState({
+      on:!this.state.on
+    })
+  }
+  render(){
+    return(
+      <div>
+        <button onClick={this.toggle}>All Students Show/Hide</button>
+        {this.state.on &&(
+          generateSquare(arr).join(",")
+        )}
+
+      </div>
+    )
+  }
+}
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {a: null,
-                  b: null};
+                  b: null,
+                click: []};
     this.ahandleChange = this.ahandleChange.bind(this);
     this.bhandleChange = this.bhandleChange.bind(this);
-    
+    this.ClickHandle = this.ClickHandle.bind(this);
 
   }
 
@@ -22,7 +47,9 @@ class App extends React.Component {
   bhandleChange(event) {
     this.setState({b: Number(event.target.value)});
   }
-
+  ClickHandle(){
+    this.setState({click:generateSquare(arr)});
+  }
   
   render() {
     rendnum++;
@@ -38,8 +65,17 @@ class App extends React.Component {
         </p>
         <p>Number a: {this.state.a}</p>
         <p>Number b: {this.state.b}</p>
-        <p>{(this.state.a>0)&&(this.state.a<this.state.b)&&(generateArray(this.state.a,this.state.b).join(","))}</p>
+        <p>{(this.state.a>0)&&(this.state.a<this.state.b)&&((arr=generateArray(this.state.a,this.state.b)).join(","))}</p>
+
+       <p>
+        <button type="button" onClick={this.ClickHandle}>Process array</button>
+        <p>{
+          this.state.click.join(",")
+        }</p>
+        </p>
+
         <p>{console.timeEnd("render - "+rendnum.toString()+" - ")}</p>
+
          </form>
     );
   }
